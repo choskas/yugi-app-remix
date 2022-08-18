45e6f85193cd2645ff603345af9129bc70fa863c
choskas <oscar.juarez@rappi.com>
2022-08-18 12:03:17 -0500

    Add more remix examples

--- app/root.jsx
+++ app/root.jsx
@@ -10,7 +10,7 @@ import styles from "./styles/app.css"
 
 export const meta = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Yugi Remix",
   viewport: "width=device-width,initial-scale=1",
 });
 
